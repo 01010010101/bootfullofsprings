47b40a79521590b473b4b4c43194c0ec857fa921
Dunkelzahn <cccp010100101@gmail.com>
2022-11-28 22:44:52 +0400

    nipple_salad

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -1,18 +1,13 @@
 package ru.kata.spring.boot_security.demo;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSecurityDemoApplication  implements ApplicationRunner {
+public class SpringBootSecurityDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
 	}
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
 
-	}
 }
