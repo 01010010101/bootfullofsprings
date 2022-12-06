dc8d0ec17f1acf1b97ff46f1d58b64cc7b7e6999
Dunkelzahn <cccp010100101@gmail.com>
2022-12-06 21:39:29 +0400

    ver. It's Alive!

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -11,7 +11,7 @@ public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private long id;
 
     public Role(String role) {
         this.role = role;
@@ -38,7 +38,7 @@ public class Role implements GrantedAuthority {
     }
 
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
